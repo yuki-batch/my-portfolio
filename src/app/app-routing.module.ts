a73bc400562281748b3f8d3e71a26636ce3c261a
yahaba <yahaba@mediamagic.co.jp>
2021-01-19 00:07:16 +0900

    業務経歴のぺージを作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {BasicInformationComponent} from './components/basic-information/basic-information.component';
 import {SkillExperienceComponent} from "./components/skill-experience/skill-experience.component";
+import {CareerComponent} from "./components/career/career.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/basic-information', pathMatch: 'full'},
   {path: 'basic-information', component: BasicInformationComponent},
-  {path: 'skill-experience', component: SkillExperienceComponent}
+  {path: 'skill-experience', component: SkillExperienceComponent},
+  {path: 'career', component: CareerComponent}
 ];
 
 @NgModule({

d634235577b0a7a0bcfc188f6288bf75180ce729
yahaba <yahaba@mediamagic.co.jp>
2021-01-25 23:54:43 +0900

    skill-experienceコンポーネントからskill-proficiency-listコンポーネントに選択中の言語を受け渡してフィルタリングをする処理を実装

--- src/app/widgets/skill-experience/skill-proficiency-list/skill-proficiency-list.component.ts
+++ src/app/widgets/skill-experience/skill-proficiency-list/skill-proficiency-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, DoCheck, Input, IterableDiffer, IterableDiffers, OnInit} from '@angular/core';
 import {SkillProficieny} from "../../../entities/skill-proficieny";
 import {ProgramingExperienceService} from "../../../services/programing-experience.service";
 
@@ -7,18 +7,47 @@ import {ProgramingExperienceService} from "../../../services/programing-experien
   templateUrl: './skill-proficiency-list.component.html',
   styleUrls: ['./skill-proficiency-list.component.css']
 })
-export class SkillProficiencyListComponent implements OnInit {
+export class SkillProficiencyListComponent implements OnInit, DoCheck {
 
   @Input() selectLanguage: string[];
-  public skillProficiency: SkillProficieny[];
+  private _selectLanguage: string[] = [];
+  private skillProficiency: SkillProficieny[];
+  public filteredSkillProficiency: SkillProficieny[] = [];
+  public selectLanguageDiffer: IterableDiffer<string>;
 
   constructor(
-    private programingExperienceService: ProgramingExperienceService
+    private programingExperienceService: ProgramingExperienceService,
+    private iterableDiffers: IterableDiffers
   ) {
   }
 
   ngOnInit() {
     this.skillProficiency = this.programingExperienceService.convertSkillProficiency();
+    this.selectLanguageDiffer = this.iterableDiffers.find(this.selectLanguage).create();
+  }
+
+  ngDoCheck() {
+    const selectLanguageChange = this.selectLanguageDiffer.diff(this.selectLanguage);
+    if (selectLanguageChange) {
+      selectLanguageChange.forEachAddedItem((record) => {
+        this._selectLanguage.push(record.item);
+      });
+
+      selectLanguageChange.forEachRemovedItem((record) => {
+        const removeIndex = this._selectLanguage.indexOf(record.item);
+        this._selectLanguage.splice(removeIndex, 1);
+      });
+
+      this.filterdSkillProficiencyList();
+    }
+  }
+
+  private filterdSkillProficiencyList() {
+    this.filteredSkillProficiency = this.skillProficiency.filter((skill) => {
+      return this._selectLanguage.some((lang) => {
+        return lang === skill.skillName;
+      });
+    });
   }
 
 }
